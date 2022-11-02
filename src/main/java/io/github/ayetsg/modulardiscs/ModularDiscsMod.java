c756f21a925568625bfd4eafaddaaced8e208d23
TSG <49595354+AyeTSG@users.noreply.github.com>
2022-11-01 23:24:13 -0400

    placeholder generated disc item

--- src/main/java/io/github/ayetsg/modulardiscs/ModularDiscsMod.java
+++ src/main/java/io/github/ayetsg/modulardiscs/ModularDiscsMod.java
@@ -1,34 +1,24 @@
+// Copyright AyeTSG 2022.
+
 package io.github.ayetsg.modulardiscs;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.resource.ResourcePack;
-import net.minecraft.resource.ResourcePackProfile;
-import net.minecraft.resource.ResourcePackSource;
-import net.minecraft.resource.ResourceType;
-import net.minecraft.resource.ZipResourcePack;
-import net.minecraft.resource.ResourcePackProfile.InsertionPosition;
-import net.minecraft.resource.metadata.PackResourceMetadata;
-import net.minecraft.text.Text;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.function.Supplier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ModularDiscsMod implements ModInitializer {
+	// setup the logger
 	public static final Logger LOGGER = LoggerFactory.getLogger("tsg_modulardiscs");
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
-
 		// get the .minecraft folder
 		Path MinecraftDir = FabricLoader.getInstance().getGameDir();
 
---
+++ src/main/java/io/github/ayetsg/modulardiscs/item/GeneratedMusicDiscItem.java
@@ -0,0 +1,14 @@
+// Copyright AyeTSG 2022.
+
+package io.github.ayetsg.modulardiscs.item;
+
+import net.minecraft.item.MusicDiscItem;
+import net.minecraft.sound.SoundEvent;
+
+public class GeneratedMusicDiscItem extends MusicDiscItem {
+
+    public GeneratedMusicDiscItem(int comparatorOutput, SoundEvent sound, Settings settings, int lengthInSeconds) {
+        super(comparatorOutput, sound, settings, lengthInSeconds);
+    }
+    
+}
