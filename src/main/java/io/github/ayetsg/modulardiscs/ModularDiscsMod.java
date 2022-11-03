8a4c5108ab11380d48e9fc4ca1a5ee221e962969
TSG <49595354+AyeTSG@users.noreply.github.com>
2022-11-02 21:46:54 -0400

    final for survival

--- src/main/java/io/github/ayetsg/modulardiscs/ModularDiscsMod.java
+++ src/main/java/io/github/ayetsg/modulardiscs/ModularDiscsMod.java
@@ -19,6 +19,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
@@ -43,31 +44,54 @@ import org.slf4j.LoggerFactory;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.mojang.datafixers.types.templates.Tag;
 
 import io.github.ayetsg.modulardiscs.item.GeneratedMusicDiscItem;
 
 public class ModularDiscsMod implements ModInitializer {
+	// store the mod id
+	public static final String MOD_ID = "tsg_modulardiscs";
+
 	// setup the logger
-	public static final Logger LOGGER = LoggerFactory.getLogger("tsg_modulardiscs");
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	// setup the internal tab render block
 	public static final Block TAB_ICON_BLOCK = new Block(FabricBlockSettings.of(Material.WOOD));
 
 	// setup the item tab
 	public static final ItemGroup GENERATED_DISC_GROUP = FabricItemGroupBuilder.build(
-		new Identifier("tsg_modulardiscs", "generated_discs"),
+		new Identifier(MOD_ID, "generated_discs"),
 		() -> new ItemStack(TAB_ICON_BLOCK));
 
 	// setup the runtime resources
-	public static final RuntimeResourcePack RESOURCE_PACK = RuntimeResourcePack.create("tsg_modulardiscs:runtime");
+	public static final RuntimeResourcePack RESOURCE_PACK = RuntimeResourcePack.create(MOD_ID + ":runtime");
 	public static final JLang FINAL_LANG = new JLang();
 	public static final JsonObject FINAL_SOUND_JSON = new JsonObject();
+	public static final JsonObject FINAL_CREEPER_LOOTTABLE_JSON = new JsonObject();
 
 	@Override
 	public void onInitialize() {
 		// register the internal tab render block
-		Registry.register(Registry.BLOCK, new Identifier("tsg_modulardiscs", "tab_icon_block"), TAB_ICON_BLOCK);
-		Registry.register(Registry.ITEM, new Identifier("tsg_modulardiscs", "tab_icon_block"), new BlockItem(TAB_ICON_BLOCK, new FabricItemSettings()));
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "tab_icon_block"), TAB_ICON_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "tab_icon_block"), new BlockItem(TAB_ICON_BLOCK, new FabricItemSettings()));
+
+		// set up starting creeper loot table
+		JsonArray discList = new JsonArray();
+		JsonObject creeperDiscDrops = new JsonObject();
+
+		// add the vanilla discs
+		discList.add("minecraft:music_disc_13");
+		discList.add("minecraft:music_disc_cat");
+		discList.add("minecraft:music_disc_blocks");
+		discList.add("minecraft:music_disc_chirp");
+		discList.add("minecraft:music_disc_far");
+		discList.add("minecraft:music_disc_mall");
+		discList.add("minecraft:music_disc_mellohi");
+		discList.add("minecraft:music_disc_stal");
+		discList.add("minecraft:music_disc_strad");
+		discList.add("minecraft:music_disc_ward");
+		discList.add("minecraft:music_disc_11");
+		discList.add("minecraft:music_disc_wait");
 
 		// get the .minecraft folder
 		Path MinecraftDir = FabricLoader.getInstance().getGameDir();
@@ -103,8 +127,8 @@ public class ModularDiscsMod implements ModInitializer {
 						String discName = discInfoJson.get("name").getAsString();
 
 						// create the resources - lang
-						FINAL_LANG.entry("item.tsg_modulardiscs." + discId, "Music Disc");
-						FINAL_LANG.entry("item.tsg_modulardiscs." + discId + ".desc", discName);
+						FINAL_LANG.entry("item." + MOD_ID + "." + discId, "Music Disc");
+						FINAL_LANG.entry("item." + MOD_ID + "." + discId + ".desc", discName);
 
 						// create the resources - models
 						ArrayList<String> fileNames = new ArrayList<>();
@@ -115,33 +139,36 @@ public class ModularDiscsMod implements ModInitializer {
 							// add the texture
 							InputStream discTex = tempZip.getInputStream(tempZip.getEntry("disc.png"));
 
-							RESOURCE_PACK.addAsset(new Identifier("tsg_modulardiscs", "textures/" + discId + ".png"), discTex.readAllBytes());
-							RESOURCE_PACK.addModel(new JModel().parent("minecraft:item/generated").textures(new JTextures().layer0("tsg_modulardiscs:" + discId)), new Identifier("tsg_modulardiscs", "item/" + discId));
+							RESOURCE_PACK.addAsset(new Identifier(MOD_ID, "textures/" + discId + ".png"), discTex.readAllBytes());
+							RESOURCE_PACK.addModel(new JModel().parent("minecraft:item/generated").textures(new JTextures().layer0(MOD_ID + ":" + discId)), new Identifier(MOD_ID, "item/" + discId));
 						} else {
 							// if we don't have a disc.png, use stal's disc texture
-							RESOURCE_PACK.addModel(new JModel().parent("minecraft:item/generated").textures(new JTextures().layer0("minecraft:item/music_disc_stal")), new Identifier("tsg_modulardiscs", "item/" + discId));
+							RESOURCE_PACK.addModel(new JModel().parent("minecraft:item/generated").textures(new JTextures().layer0("minecraft:item/music_disc_stal")), new Identifier(MOD_ID, "item/" + discId));
 						}
 
 						// create the resources - sound
-						RESOURCE_PACK.addAsset(new Identifier("tsg_modulardiscs", "sounds/" + discId + ".ogg"), tempZip.getInputStream(tempZip.getEntry("disc.ogg")).readAllBytes());
+						RESOURCE_PACK.addAsset(new Identifier(MOD_ID, "sounds/" + discId + ".ogg"), tempZip.getInputStream(tempZip.getEntry("disc.ogg")).readAllBytes());
 
 						// create the resources - sound event
 						JsonObject soundData = new JsonObject();
 						JsonArray soundsList = new JsonArray();
 						JsonObject soundPointer = new JsonObject();
-						soundPointer.addProperty("name", "tsg_modulardiscs:" + discId);
+						soundPointer.addProperty("name", MOD_ID + ":" + discId);
 						soundPointer.addProperty("stream", true);
 						soundsList.add(soundPointer);
 						soundData.add("sounds", soundsList);
 						FINAL_SOUND_JSON.add(discId, soundData);
 
+						// create the resources - creeper loot table
+						discList.add(MOD_ID + ":" + discId);
+
 						// register the sound event with fabric
-						final Identifier GENERATED_SOUND_EVENT_ID = new Identifier("tsg_modulardiscs", discId);
+						final Identifier GENERATED_SOUND_EVENT_ID = new Identifier(MOD_ID, discId);
 						final SoundEvent GENERATED_SOUND_EVENT = new SoundEvent(GENERATED_SOUND_EVENT_ID);
 
 						// create the item
 						final Item GENERATED_DISC = new GeneratedMusicDiscItem(0, GENERATED_SOUND_EVENT, new FabricItemSettings().group(GENERATED_DISC_GROUP).rarity(Rarity.RARE).maxCount(1), 0);
-						Registry.register(Registry.ITEM, new Identifier("tsg_modulardiscs", discId), GENERATED_DISC);
+						Registry.register(Registry.ITEM, new Identifier(MOD_ID, discId), GENERATED_DISC);
 
 						// close the temporary zip, it isnt needed anymore.
 						tempZip.close();
@@ -156,16 +183,25 @@ public class ModularDiscsMod implements ModInitializer {
 		// LOGGER.info(FINAL_SOUND_JSON.toString());
 
 		// register the resource pack
-		RESOURCE_PACK.addLang(new Identifier("tsg_modulardiscs:en_us"), FINAL_LANG);
+		RESOURCE_PACK.addLang(new Identifier(MOD_ID + ":en_us"), FINAL_LANG);
+
+		FINAL_CREEPER_LOOTTABLE_JSON.add("values", discList);
 
 		try {
-			RESOURCE_PACK.addAsset(new Identifier("tsg_modulardiscs", "sounds.json"), FINAL_SOUND_JSON.toString().getBytes("UTF-8"));
+			RESOURCE_PACK.addData(new Identifier("minecraft", "tags/items/creeper_drop_music_discs.json"), FINAL_CREEPER_LOOTTABLE_JSON.toString().getBytes("UTF-8"));
 		} catch (UnsupportedEncodingException e) {
 			LOGGER.error("Unsupported encoding!");
 			LOGGER.error(e.toString());
 		}
 
-		// RESOURCE_PACK.dumpDirect(Paths.get(MinecraftDir.toString(), "DEBUG"));
+		try {
+			RESOURCE_PACK.addAsset(new Identifier(MOD_ID, "sounds.json"), FINAL_SOUND_JSON.toString().getBytes("UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			LOGGER.error("Unsupported encoding!");
+			LOGGER.error(e.toString());
+		}
+
+		RESOURCE_PACK.dumpDirect(Paths.get(MinecraftDir.toString(), "DEBUG"));
 
 		RRPCallback.AFTER_VANILLA.register(a -> a.add(RESOURCE_PACK));
 	}
