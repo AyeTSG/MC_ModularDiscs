144e0463033d72b3d932d57fb00573fd49227c6c
TSG <49595354+AyeTSG@users.noreply.github.com>
2022-11-02 20:30:05 -0400

    redundant

--- src/main/java/io/github/ayetsg/modulardiscs/ModularDiscsMod.java
+++ src/main/java/io/github/ayetsg/modulardiscs/ModularDiscsMod.java
@@ -19,7 +19,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
